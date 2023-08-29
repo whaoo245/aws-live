245e006c98a20fb7b08aca7cc78df9c41e8a58f3
Wei Hao <85218293+whaoo245@users.noreply.github.com>
2023-08-29 15:40:13 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cwxbc5axuqci.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "fongweihao-employee"
+customregion = "us-east-1"
+
